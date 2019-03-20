057839fb8c003996dc8acead3c7e38238cc1f1d3
X-olani <xolani.sk.yona@gmail.com>
2019-03-21 00:09:42 +0200

    new  new commit

---
+++ build/precache-manifest.3abe928e5e4945805e036a4b7c7b59b9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e53edc51429bcdf805deee1de8009c6",
+    "url": "/static/media/theone.0e53edc5.jpeg"
+  },
+  {
+    "revision": "6e57be88b2bd34398278185ad30b927e",
+    "url": "/static/media/pexels-photo-169976.6e57be88.jpeg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fe4af3d78da1e583a6d7361cf4b0f9d9",
+    "url": "/static/media/Pegasus-PNG-Photos.fe4af3d7.png"
+  },
+  {
+    "revision": "2254d5f6f9c11121b65a0b5766a1315d",
+    "url": "/static/media/1003868.2254d5f6.jpeg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "774efffabf76cac1a2c9",
+    "url": "/static/js/main.774efffa.chunk.js"
+  },
+  {
+    "revision": "454fadc43c56ca7b5e1c",
+    "url": "/static/js/1.454fadc4.chunk.js"
+  },
+  {
+    "revision": "774efffabf76cac1a2c9",
+    "url": "/static/css/main.71b9596a.chunk.css"
+  },
+  {
+    "revision": "3734f61a6090c5c97858106610f63c84",
+    "url": "/index.html"
+  }
+];
